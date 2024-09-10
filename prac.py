1f426749d9edec3f31113bf83c7a792f23247ea6
Paarth Saxena <paarthsaxena2005@gmail.com>
2024-09-10 19:31:21 +0530

    Removing the Unnecessary file from Git

--- prac.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-
-localconnection = sqlite3.connect('LoginCredentials.db')
-cursor = localconnection.cursor()
-
-cursor.execute("update users set balance = earned - payout where email = ?" , ('paarthsaxena2005@gmail.com',))
